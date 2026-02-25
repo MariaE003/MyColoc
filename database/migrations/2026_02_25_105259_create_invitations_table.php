788dd6dd5d80c89739b0e7d20739533141ab1f52
MariaE003 <elgotbymaria@gmail.com>
2026-02-25 11:21:19 +0000

    creer la migration du table invitaion

---
+++ database/migrations/2026_02_25_105259_create_invitations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('colocation_id');
+            $table->unsignedBigInteger('sender_id');   
+            $table->unsignedBigInteger('receiver_id'); 
+            $table->string('token')->unique();
+            $table->enum('status',['en attente','accepter','refuser'])->default('pending');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invitations');
+    }
+};
